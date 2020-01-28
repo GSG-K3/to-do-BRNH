cfecd12928c476ce35a100243a6c5dccd703fe60
bayanseder <bayansider88@gmail.com>
2020-01-28 16:07:08 +0200

    deleting task function #2

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 var test = require('tape');
 var logic = require('./logic');
-
+console.log(logic["deleteTodo"]);
 test('Example test', function(t) {
   t.pass();
   t.end();
 });
+var test_arr=[{'id':0,'description': 'sport', 'done':false},{'id':1,'description': 'eat lunch', 'done':false},{'id':2,'description': 'homework', 'done':false}];
+test('Delete task test', function(t){
+    t.equal((logic["deleteTodo"](test_arr, 0)).length,test_arr.length-1,'To-do list length should dectrement by 1');
+    t.equal((logic["deleteTodo"](test_arr, 2)).length,test_arr.length-1,'To-do list length should dectrement by 1');
+    t.equal( null,null,'You should select an task to Delete');
+    t.equal([].length,0,'You should select an task to Delete');
+    t.end();
+});
+//logic.addTodo(dsdf,hgchgh),
