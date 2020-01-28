bee8753fda961353062da6e24e23f7d5cd610142
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-01-28 16:09:04 +0200

    test addTodo function #1

--- test.js
+++ test.js
@@ -1,7 +1,53 @@
-var test = require('tape');
-var logic = require('./logic');
+let test = require('tape');
+var todoFunctions = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
-});
+let ex1 = [{
+  id: 1,
+  description: "play",
+  done: false,
+}
+
+];
+
+
+let ex2 = [{
+  id: 1,
+  description: "play",
+  done: false,
+},
+{
+  id: 2,
+  description: "eat",
+  done: false,
+}
+
+];
+
+let ex3 = [{
+  id: 1,
+  description: "play",
+  done: false,
+},
+{
+  id: 2,
+  description: "eat",
+  done: false,
+},
+{
+  id: 3,
+  description: "die",
+  done: false,
+}
+
+];
+
+
+  test('test add todo', t => {
+    t.deepEqual( todoFunctions.addTodo([],{ description: 'play' }), ex1 , 'the array from AddFunction should be equal the expected Array');
+    t.deepEqual( todoFunctions.addTodo(ex1,{ description: 'eat' }), ex2, 'the array from AddFunction should be equal the expected Array');
+    t.deepEqual( todoFunctions.addTodo(ex2,{ description: 'die' }), ex3, 'the array from AddFunction should be equal the expected Array');
+
+    t.end();
+  } )
+
+ 
