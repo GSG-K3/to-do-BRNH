08d214d846c622515755cc5a659f43811e5cfb06
bayanseder <bayansider88@gmail.com>
2020-01-29 14:39:01 +0200

    adding more style

--- dom.js
+++ dom.js
@@ -1,72 +1,81 @@
-// part 2 linking it all together
-// The function here is called an iife,
-// it keeps everything inside hidden from the rest of our application
 (function() {
-    // This is the dom node where we will keep our todo
-    var container = document.getElementById('todo-container');
-    var addTodoForm = document.getElementById('add-todo');
+
+  // This is the dom node where we will keep our todo
+  var container = document.getElementById('todo-container');
+  var addTodoForm = document.getElementById('add-todo');
+  var input = document.getElementById("description");
   
-    var state = [
-      { id: -3, description: 'first todo' },
-      { id: -2, description: 'second todo' },
-      { id: -1, description: 'third todo' },
-    ]; // this is our initial todoList
-  
-    // This function takes a todo, it returns the DOM node representing that todo
-    var createTodoNode = function(todo) {
-      var todoNode = document.createElement('li');
-      // you will need to use addEventListener
-  
-      // add span holding description
-  
-      // this adds the delete button
-      var deleteButtonNode = document.createElement('button');
-      deleteButtonNode.className="deletebutton";
-      deleteButtonNode.addEventListener('click', function(event) {
-        var newState = todoFunctions.deleteTodo(state, todo.id);
-        update(newState);
-      });
-      todoNode.appendChild(deleteButtonNode);
-  
-      // add markTodo button
-  
-      // add classes for css
-      
-      return todoNode;
-    };
-  
-    // bind create todo form
-    if (addTodoForm) {
-      addTodoForm.addEventListener('submit', function(event) {
-        // https://developer.mozilla.org/en-US/docs/Web/Events/submit
-        // what does event.preventDefault do?
-        // what is inside event.target?
-  
-        var description = '?'; // event.target ....
-  
-        // hint: todoFunctions.addTodo
-        var newState = []; // ?? change this!
-        update(newState);
-      });
+// this is our initial todoList
+  let state = [
+    { id: -3, description: 'first todo', done: false},
+    { id: -2, description: 'second todo', done: false },
+    { id: -1, description: 'third todo', done: false },
+  ]; 
+
+  // This function takes a todo, it returns the DOM node representing that todo
+  var createTodoNode = function(todo) {
+    var todoNode = document.createElement('li');
+    todoNode.innerHTML = todo.description;
+    todoNode.classList.add("list-style") 
+
+    
+
+    // this adds the delete button
+    var deleteButtonNode = document.createElement('button');
+    deleteButtonNode.innerHTML = ""
+    deleteButtonNode.classList.add("deletebutton")
+    deleteButtonNode.addEventListener('click', function(event) {
+      var newState = todoFunctions.deleteTodo(state, todo.id);
+      update(newState);
+    });
+
+    todoNode.appendChild(deleteButtonNode);
+
+      // this adds the mark button
+     let markButtonNode = document.createElement('button');
+     markButtonNode.classList.add("markbutton")
+     markButtonNode.addEventListener('click', function(event) {
+       var newState = todoFunctions.markTodo(state, todo.id);
+       update(newState);
+     });
+     let buttonsDiv = document.createElement('div')
+     buttonsDiv.appendChild(deleteButtonNode);
+     buttonsDiv.appendChild(markButtonNode);
+
+      todoNode.appendChild(buttonsDiv)
+
+     if(todo.done) {
+      todoNode.classList.add("mark")
     }
-  
-    // you should not need to change this function
-    var update = function(newState) {
-      state = newState;
-      renderState(state);
-    };
-  
-    // you do not need to change this function
-    var renderState = function(state) {
-      var todoListNode = document.createElement('ul');
-  
-      state.forEach(function(todo) {
-        todoListNode.appendChild(createTodoNode(todo));
-      });
-  
-      // you may want to add a class for css
-      container.replaceChild(todoListNode, container.firstChild);
-    };
-  
-    if (container) renderState(state);
-  })();
+
+    return todoNode;
+  };
+
+  // bind create todo form
+  if (addTodoForm) {
+    addTodoForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      let description = {description: input.value};
+     let newState = todoFunctions.addTodo(state, description ); 
+      update(newState);
+      input.value = "";
+    });
+  }
+
+  var update = function(newState) {
+    state = newState;
+    renderState(state);
+  };
+
+  var renderState = function(state) {
+    var todoListNode = document.createElement('ul');
+
+    state.forEach(function(todo) {
+      todoListNode.appendChild(createTodoNode(todo));
+    });
+
+    container.replaceChild(todoListNode, container.firstChild);
+  };
+
+  if (container) renderState(state);
+})();
--- test.js
+++ test.js
@@ -41,22 +41,59 @@ let ex3 = [{
 
 ];
 
+let marked = [{
+  id: 1,
+  description: "play",
+  done: false,
+},
+{
+  id: 2,
+  description: "eat",
+  done: true,
+}
 
+];
+
+let marked2 = [{
+  id: 1,
+  description: "play",
+  done: true,
+},
+{
+  id: 2,
+  description: "eat",
+  done: false,
+}
+
+];
+
+// Add Test
   test('test add todo', t => {
     t.deepEqual( logic.addTodo([],{ description: 'play' }), ex1 , 'the array from AddFunction should be equal the expected Array');
     t.deepEqual( logic.addTodo(ex1,{ description: 'eat' }), ex2, 'the array from AddFunction should be equal the expected Array');
     t.deepEqual( logic.addTodo(ex2,{ description: 'die' }), ex3, 'the array from AddFunction should be equal the expected Array');
+    t.deepEqual ( logic.addTodo("string",{ description: 'die' }), 'the first argument should be array', "if the first argument is array");
 
     t.end();
   } )
 
- 
+  // Mark Test
+  test('test todo mark ', t => {
+    t.deepEqual( logic.markTodo(ex2,2), marked , 'the array from MarkFunction should be equal the expected Array');
+    t.deepEqual( logic.markTodo(ex2,1), marked2 , 'the array from MarkFunction should be equal the expected Array');
+    t.deepEqual ( logic.markTodo([],1), 'you should enter a task first', 'if the array is empty');
+    t.deepEqual ( logic.markTodo("string",1), 'the first argument should be array and the second argument should be number', "if the input arguments are true");
 
+    t.end();
+  } )
+ 
+  // Delete Test
 var test_arr=[{'id':0,'description': 'sport', 'done':false},{'id':1,'description': 'eat lunch', 'done':false},{'id':2,'description': 'homework', 'done':false}];
 test('Delete task test', function(t){
     t.equal((logic["deleteTodo"](test_arr, 0)).length,test_arr.length-1,'To-do list length should dectrement by 1');
     t.equal((logic["deleteTodo"](test_arr, 2)).length,test_arr.length-1,'To-do list length should dectrement by 1');
-    t.equal( null,null,'You should select an task to Delete');
-    t.equal([].length,0,'You should select an task to Delete');
+    
     t.end();
 });
+
+
